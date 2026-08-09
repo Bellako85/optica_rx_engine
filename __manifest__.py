e704d21f41718d4919db5a192b12290e9a073991
Christian <hhvida@gmail.com>
2026-08-09 02:20:50 -0600

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Optica RX Engine',
+    'version': '17.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Motor de reglas para cotización óptica',
+    'author': 'Optica Zamora',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+        'product',
+    ],
+    'installable': True,
+    'application': False,
+}
