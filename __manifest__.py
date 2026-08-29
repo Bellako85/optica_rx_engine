0aeb9fdd87585fce3cb91fe03540cd0abd41cbb7
Christian <hhvida@gmail.com>
2026-08-28 19:21:21 -0600

    Add CSS asset for backend in manifest
    el assets al manifest

--- __manifest__.py
+++ __manifest__.py
@@ -18,6 +18,12 @@
         'views/cotizador_wizard_views.xml',
         'views/product_template_views.xml',
     ],
+
+    'assets': {
+        'web.assets_backend': [
+            'optica_rx_engine/static/src/css/cotizador_wizard.css',
+    ],
+},
     
     'installable': True,
     'application': True,
